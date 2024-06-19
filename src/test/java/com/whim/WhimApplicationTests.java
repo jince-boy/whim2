a69ecd74da4b127cd240117ea4b7e378b6fbc559
Jin Ce <jince_hm@163.com>
2024-06-19 22:56:34 +0800

    feat: init whim

---
+++ src/test/java/com/whim/WhimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
