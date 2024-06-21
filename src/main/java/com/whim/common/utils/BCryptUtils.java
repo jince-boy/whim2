9630b4b7b66cc4c9f153521df52a03fa181cf85d
Jin Ce <jince_hm@163.com>
2024-06-21 23:42:25 +0800

    feat: add BCryptUtils BCrypt密码加密工具类

---
+++ src/main/java/com/whim/common/utils/BCryptUtils.java
@@ -0,0 +1,31 @@
+package com.whim.common.utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+/**
+ * @author Jince
+ * date: 2024/6/21 下午11:36
+ * description: BCrypt 密码加密工具类
+ */
+public class BCryptUtils {
+    private static final int DEFAULT_COST = 10;
+
+    /**
+     * BCrypt编码
+     *
+     * @param str 字符串
+     * @return 编码后的字符串
+     */
+    public static String encode(String str) {
+        return BCrypt.hashpw(str, BCrypt.gensalt(DEFAULT_COST));
+    }
+
+    /**
+     * @param rawStr     原字符串
+     * @param encodedStr 编码后的字符串
+     * @return true正确，false不正确
+     */
+    public static boolean matches(String rawStr, String encodedStr) {
+        return BCrypt.checkpw(rawStr, encodedStr);
+    }
+}
