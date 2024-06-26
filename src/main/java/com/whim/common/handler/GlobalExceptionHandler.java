9c8132158072fbe9e255c09b15ed5f317810e228
JinCe <jince_hm@163.com>
2024-06-26 18:37:45 +0800

    feat: add Exception

--- src/main/java/com/whim/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/whim/common/handler/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package com.whim.common.handler;
 
 import cn.dev33.satoken.exception.NotLoginException;
 import cn.dev33.satoken.exception.NotPermissionException;
+import com.whim.common.exception.CheckCaptchaException;
 import com.whim.common.exception.InvalidParameterException;
 import com.whim.common.exception.ServiceException;
+import com.whim.common.exception.UserNotFoundException;
+import com.whim.common.exception.UserPasswordNotMatchException;
 import com.whim.common.web.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -52,5 +55,32 @@ public class GlobalExceptionHandler {
     public Result<String> handleNotPermissionException(NotPermissionException exception) {
         log.error(exception.getMessage());
         return Result.error(HttpStatus.FORBIDDEN, "用户没有权限");
+    }
+
+    /**
+     * 验证码异常
+     */
+    @ExceptionHandler(CheckCaptchaException.class)
+    public Result<String> handleCheckCaptchaException(CheckCaptchaException exception) {
+        log.error(exception.getMessage());
+        return Result.argumentError(exception.getMessage());
+    }
+
+    /**
+     * 用户名密码错误
+     */
+    @ExceptionHandler(UserPasswordNotMatchException.class)
+    public Result<String> handleUserPasswordNotMatchException(UserPasswordNotMatchException exception) {
+        log.error(exception.getMessage());
+        return Result.unauthorized(exception.getMessage());
+    }
+
+    /**
+     * 用户不存在
+     */
+    @ExceptionHandler(UserNotFoundException.class)
+    public Result<String> handleUserNotFoundException(UserNotFoundException exception) {
+        log.error(exception.getMessage());
+        return Result.error(HttpStatus.NOT_FOUND, exception.getMessage());
     }
 }
