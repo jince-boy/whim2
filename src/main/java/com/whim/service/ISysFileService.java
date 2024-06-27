914910f20a9f366d5f2a704530aaf857f157ac2d
Jin Ce <jince_hm@163.com>
2024-06-27 22:04:50 +0800

    feat: add SysFile

---
+++ src/main/java/com/whim/service/ISysFileService.java
@@ -0,0 +1,14 @@
+package com.whim.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.whim.pojo.entity.SysFile;
+
+/**
+ * 系统文件(SysFile)表服务接口
+ *
+ * @author JinCe
+ * @since 2024-06-27 22:03:31
+ */
+public interface ISysFileService extends IService<SysFile> {
+
+}
