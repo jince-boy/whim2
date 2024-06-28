4b4f6befa3cd73467acc0e9540b06c5464c144ad
Jin Ce <jince_hm@163.com>
2024-06-28 23:16:32 +0800

    feat: update Spring Boot multi-module modularization

---
+++ whim-system/src/main/java/com/whim/Main.java
@@ -0,0 +1,12 @@
+package com.whim;
+
+/**
+ * @author Jince
+ * date: 2024/6/28 下午11:14
+ * description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
