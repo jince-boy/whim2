cadb4d861798604ae7f9ecbd51f03a9c20b164ba
JinCe <jince_hm@163.com>
2024-08-21 18:03:13 +0800

    feat: 重构文件模块

---
+++ whim-file/src/main/java/com/whim/service/FileStorage.java
@@ -0,0 +1,10 @@
+package com.whim.service;
+
+/**
+ * @author jince
+ * date: 2024/8/21 上午11:10
+ * description: 文件存储接口
+ */
+public interface FileStorage extends AutoCloseable {
+
+}
---
+++ whim-file/src/main/java/com/whim/service/FileStorageService.java
@@ -0,0 +1,13 @@
+package com.whim.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jince
+ * date: 2024/8/21 下午4:01
+ * description: 文件存储服务
+ */
+@Component
+public class FileStorageService {
+//    public
+}
